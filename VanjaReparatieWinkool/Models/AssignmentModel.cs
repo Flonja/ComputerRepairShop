4cf17cb1d3fcfaca946e099733e9f5f55ea96b8e
Flonja <vanjavanessen@icloud.com>
2020-10-02 14:08:06 +0200

    Add files via upload

---
+++ VanjaReparatieWinkool/Models/AssignmentModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace VanjaReparatieWinkool.Models
+{
+    public class AssignmentModel
+    {
+        [Key]
+        public int AssignmentId { get; set; }
+        public string Omschrijving { get; set; }
+        public Status Status { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime StartDatum { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime EindDatum { get; set; }
+        public virtual CustomerModel Klant { get; set; }
+        [NotMapped]
+        public decimal Prijs { get; set; }
+        public double Uren { get; set; }
+        public virtual EmployeeModel Werknemer { get; set; }
+        public virtual ICollection<PartModel> Onderdelen { get; set; }
+    }
+}
---
+++ VanjaReparatieWinkool/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VanjaReparatieWinkool.Models
+{
+    public class EmployeeModel : UserModel
+    {
+        //public EmployeeModel()
+        //{
+        //    this.Assignments = new HashSet<AssignmentModel>();
+        //}
+
+        [Key]
+        public int EmployeeId { get; set; }
+
+        public virtual ICollection<AssignmentModel> Assignments { get; set; }
+    }
+}
