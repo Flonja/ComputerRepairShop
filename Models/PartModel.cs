1bde461a2b11fc454580b69ac7e6977c09c49877
Flonja <vanjavanessen@icloud.com>
2020-10-03 11:54:01 +0200

    Add files via upload

---
+++ Models/PartModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VanjaReparatieWinkool.Models
+{
+    public class PartModel
+    {
+        [Key]
+        public int PartId { get; set; }
+        public string Naam { get; set; }
+        public string Leverancier { get; set; }
+        public int Aantal { get; set; }
+        public decimal Prijs { get; set; }
+        public virtual ICollection<AssignmentModel> Assignments { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VanjaReparatieWinkool.Models
+{
+    public class UserModel
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Adres { get; set; }
+        public string Postcode { get; set; }
+        public string Provincie { get; set; }
+    }
+}
